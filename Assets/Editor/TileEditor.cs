f8c86048f6aa8104964b34e816d49d06d82b37c7
Beaudmat <97978688+Beaudmat@users.noreply.github.com>
2024-05-09 16:57:34 -0400

    Editor Addition and Removal
    Added the ability in the editor for Designers to delete selected tiles and to add tiles onto the grid via the coordinates they give.

--- Assets/Editor/TileEditor.cs
+++ Assets/Editor/TileEditor.cs
@@ -20,6 +20,9 @@ public class TileEditor : EditorWindow
     private int popupIndex = 0;
     private GameObject selectedPrefab;
 
+    //Variables for adding tiles
+    private Vector2 placementPosition = Vector2.zero;
+
     #endregion
 
     #region EditorMethods
@@ -72,23 +75,21 @@ public class TileEditor : EditorWindow
         }
         EditorGUILayout.LabelField("", GUI.skin.horizontalSlider);
 
-        /*
-         * TILE REPLACING SECTION
-         */
-        GUILayout.Label("Tile Replacement Controls:", EditorStyles.boldLabel);
-        GUILayout.Space(5);
-
         //Gets the List of Prefabs the user wants to use while replacing
         prefabTilesList = (GameObject)EditorGUILayout.ObjectField("Prefab Tiles List: ", prefabTilesList, typeof(GameObject), true);
         GUILayout.Space(10);
 
         //Pulls the prefab names from the provided list
         List<string> prefabNames = new List<string>();
-        foreach(Transform prefab in prefabTilesList.transform.GetComponentInChildren<Transform>()) 
+        if(prefabTilesList != null)
         {
-            prefabNames.Add(prefab.name);
+            foreach (Transform prefab in prefabTilesList.transform.GetComponentInChildren<Transform>())
+            {
+                prefabNames.Add(prefab.name);
+            }
         }
 
+        GUILayout.Label("Tile Prefab Selection: ");
         //Checks what index the user has selected in the dropdown menu
         popupIndex = EditorGUILayout.Popup(popupIndex, prefabNames.ToArray());
 
@@ -97,25 +98,91 @@ public class TileEditor : EditorWindow
         {
             selectedPrefab = prefabTilesList.transform.GetChild(popupIndex).gameObject;
         }
-        GUILayout.Space(10);
+        GUILayout.Space(5);
+
+        /*
+         * TILE REPLACING SECTION
+         */
+        GUILayout.Label("Tile Replacement Controls:", EditorStyles.boldLabel);
+        GUILayout.Space(5);
 
         //Replaces the tiles based on selection
-        if (GUILayout.Button("Replace Selected Tile"))
+        if (GUILayout.Button("Replace Selected Tiles"))
         {
             //Checks if there are any objects selected
             GameObject selected = (GameObject)Selection.activeObject;
-            if(selected != null)
+            if(selectedPrefab == null)
             {
-                if (selected.GetComponent<Tile>() != null)
+                EditorUtility.DisplayDialog("Tile Editor Error", "You need to select a replacement prefab before you can replace.", "Confirm");
+            }
+            else
+            {
+                if (selected != null)
                 {
-                    //Replaces the selected Tiles
-                    ReplaceTile();
+                    if (selected.GetComponent<Tile>() != null)
+                    {
+                        //Replaces the selected Tiles
+                        ReplaceTile();
+                    }
+                    else
+                    {
+                        //Throws an error if the thing they are trying to replace isn't a tile
+                        EditorUtility.DisplayDialog("Tile Editor Error", "Object you tried to replace doesn't have a tile component", "Confirm");
+                    }
                 }
-                else
+            }
+        }
+        GUILayout.Space(5);
+
+        /*
+         * TILE DELETING SECTION
+         */
+        GUILayout.Label("Tile Deletion Controls:", EditorStyles.boldLabel);
+        GUILayout.Space(5);
+
+        //Deletes the selected tiles
+        if (GUILayout.Button("Delete Selected Tiles"))
+        {
+            if (EditorUtility.DisplayDialog("Tile Replacement", "Are you sure you want to remove the selected tiles?", "Confirm", "Cancel"))
+            {
+                //Checks if there are any objects selected
+                GameObject selected = (GameObject)Selection.activeObject;
+                if (selected != null)
                 {
-                    //Throws an error if the thing they are trying to replace isn't a tile
-                    EditorUtility.DisplayDialog("Tile Editor Error", "Object you tried to replace doesn't have a tile component", "Confirm");
+                    if (selected.GetComponent<Tile>() != null)
+                    {
+                        //Replaces the selected Tiles
+                        RemoveTile();
+                    }
+                    else
+                    {
+                        //Throws an error if the thing they are trying to replace isn't a tile
+                        EditorUtility.DisplayDialog("Tile Editor Error", "Object you tried to remove doesn't have a tile component", "Confirm");
+                    }
                 }
+            }
+        }
+        EditorGUILayout.LabelField("", GUI.skin.horizontalSlider);
+
+        /*
+         * TILE ADDING SECTION
+         */
+        GUILayout.Label("Tile Addition Controls:", EditorStyles.boldLabel);
+        GUILayout.Space(5);
+
+        //Grid Start Position
+        placementPosition = EditorGUILayout.Vector2Field("Tile Addition Pos: ", placementPosition);
+        GUILayout.Space(5);
+
+        if(GUILayout.Button("Add Tile at selected location"))
+        {
+            if (selectedPrefab != null)
+            {
+                AddTile();
+            }
+            else
+            {
+                EditorUtility.DisplayDialog("Tile Editor Error", "You need to select a replacement prefab before you can replace.", "Confirm");
             }
         }
     }
@@ -173,6 +240,70 @@ public class TileEditor : EditorWindow
                 TilesToDestroy.RemoveAt(0);
             }
         }
+    }
+
+    private void RemoveTile()
+    {
+        //Tracks the currently selected tile
+        GameObject selectedObject;
+
+        //Used for multi select for destroying tiles
+        List<GameObject> TilesToDestroy = new List<GameObject>();
+
+        //If only a single tile is selected
+        if (Selection.objects.Length < 1)
+        {
+            //Grabs the selected tile
+            selectedObject = (GameObject)Selection.activeObject;
+
+            //Destroys the old tile
+            DestroyImmediate(selectedObject);
+        }
+        else
+        {
+            //Runs through all the selected objects
+            foreach (object tile in Selection.objects)
+            {
+                selectedObject = (GameObject)tile;
+                //Checks if the selected object is a tile
+                if (selectedObject.transform.GetComponent<Tile>() != null)
+                {
+                    //Inserts the old tile into a list of tils to destroy
+                    TilesToDestroy.Add(selectedObject);
+                }
+            }
+
+            //Runs through the list of tiles to destroy and gets rid of them
+            while (TilesToDestroy.Count > 0)
+            {
+                DestroyImmediate(TilesToDestroy[0]);
+                TilesToDestroy.RemoveAt(0);
+            }
+        }
+    }
+
+    private void AddTile()
+    {
+        if(gridParent != null)
+        {
+            Transform gridTransform = gridParent.transform;
+
+            Vector2 tileSize = DetermineTileSize(selectedPrefab.GetComponent<MeshFilter>().sharedMesh.bounds);
+            Vector3 position = gridTransform.position;
+
+            position.x = gridTransform.position.x + tileSize.x * placementPosition.x;
+            position.z = gridTransform.position.z + tileSize.y * placementPosition.y;
+
+            position.z += DetermineOffset(placementPosition.x, tileSize.y);
+
+            GenerateTile(selectedPrefab, position, new Vector2Int((int)placementPosition.x, (int)placementPosition.y));
+        }
+        else
+        {
+            EditorUtility.DisplayDialog("Tile Editor Error", "No grid created to add onto", "Confirm");
+        }
+
+
     }
 
     #endregion
@@ -240,7 +371,7 @@ public class TileEditor : EditorWindow
     {
         GameObject newTile = Instantiate(tile.gameObject, pos, Quaternion.identity);
         newTile.transform.parent = gridParent.transform;
-        newTile.name = "Tile " + id;
+        newTile.name = id.ToString();
     }
 
     #endregion
