1e6f3834e9d08a95b7de86e3d563833c68d56121
Aria Lian <arialian0829@gmail.com>
2024-05-30 18:29:54 -0400

    HealthBar scale with camera

--- Assets/Game/Scripts/UI/LookAtCamera.cs
+++ Assets/Game/Scripts/UI/LookAtCamera.cs
@@ -5,6 +5,9 @@ using UnityEngine;
 public class LookAtCamera : MonoBehaviour
 {
     private Camera mainCamera;
+    public Vector3 minScale = Vector3.one * 0.1f; 
+    public Vector3 maxScale = Vector3.one;
+    public float factor = 0.02f;
 
     private void Start()
     {
@@ -14,5 +17,10 @@ public class LookAtCamera : MonoBehaviour
     private void LateUpdate()
     {
         transform.forward = Camera.main.transform.forward;
+
+        // Scale based on distance from camera
+        float distance = Vector3.Distance(transform.position, mainCamera.transform.position);
+        float scale = distance * factor;
+        transform.localScale = Vector3.Lerp(minScale, maxScale, scale);
     }
 }
