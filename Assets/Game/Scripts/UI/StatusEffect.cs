719d0b43168031a431aac2e710902f9ffb89e215
Aria Lian <arialian0829@gmail.com>
2024-06-21 12:55:09 -0400

    Modified Tile Effect

--- Assets/Game/Scripts/UI/StatusEffect.cs
+++ Assets/Game/Scripts/UI/StatusEffect.cs
@@ -13,6 +13,8 @@ public class StatusEffect : MonoBehaviour
 
     public void Initialize(Status status)
     {
+        transform.localScale = new Vector3(1, 1, 1);  // for fixing scale difference in different resolutions
+
         icon.sprite = characterUIConfig.GetStatusSprite(status);
         effectTurns.text = status.effectTurns.ToString();
         effectDetail.text = characterUIConfig.GetStatusExplain(status);
--- Assets/Game/Scripts/UI/TileEffect.cs
+++ Assets/Game/Scripts/UI/TileEffect.cs
@@ -8,10 +8,21 @@ public class TileEffect : MonoBehaviour
 {
     public GameObject panel;
     public Image icon;
-    public TextMeshProUGUI text;
+    [SerializeField] private TextMeshProUGUI text;
+    [SerializeField] private TextMeshProUGUI detail;
 
     [SerializeField] private Sprite buffSprite;
     [SerializeField] private Sprite debuffSprite;
+
+    [Header("Tile Buff/Debuff")]
+    [SerializeField] private KeywordDescription waterTileBuff;
+    [SerializeField] private KeywordDescription waterTileDebuff;
+    [SerializeField] private KeywordDescription fireTileBuff;
+    [SerializeField] private KeywordDescription fireTileDebuff;
+    [SerializeField] private KeywordDescription grassTileBuff;
+    [SerializeField] private KeywordDescription grassTileDebuff;
+    [SerializeField] private KeywordDescription poisonTileDebuff;
+
 
     public void SetEffect(ElementType characterType, ElementType tileType)
     {
@@ -19,17 +30,82 @@ public class TileEffect : MonoBehaviour
         {
             panel.SetActive(false);
         }
-        else if (characterType == tileType)
+        else
+        {
+            panel.SetActive(true);
+
+            switch (tileType)
+            {
+                case ElementType.Water:
+                    SetWaterEffect(characterType);
+                    break;
+                case ElementType.Fire:
+                    SetFireEffect(characterType);
+                    break;
+                case ElementType.Grass:
+                    SetGrassEffect(characterType);
+                    break;
+                case ElementType.Poison:
+                    SetPoisonEffect();
+                    break;
+            }
+
+            detail.ForceMeshUpdate();
+        }
+    }
+
+    private void SetPoisonEffect()
+    {
+        icon.sprite = debuffSprite;
+        text.text = "Poison";
+        detail.text = poisonTileDebuff.DisplayKeywordDescription();
+    }
+
+    private void SetWaterEffect(ElementType characterType)
+    {
+        if (characterType == ElementType.Water)
         {
             icon.sprite = buffSprite;
-            text.text = "Character can get a Buff here";
-            panel.SetActive(true);
+            text.text = "Buff";
+            detail.text = waterTileBuff.DisplayKeywordDescription();
         }
         else
         {
             icon.sprite = debuffSprite;
-            text.text = "Character can get a Debuff here";
-            panel.SetActive(true);
+            text.text = "Debuff";
+            detail.text = waterTileDebuff.DisplayKeywordDescription();
+        }
+    }
+
+    private void SetFireEffect(ElementType characterType)
+    {
+        if (characterType == ElementType.Fire)
+        {
+            icon.sprite = buffSprite;
+            text.text = "Buff";
+            detail.text = fireTileBuff.DisplayKeywordDescription();
+        }
+        else
+        {
+            icon.sprite = debuffSprite;
+            text.text = "Debuff";
+            detail.text = fireTileDebuff.DisplayKeywordDescription();
+        }
+    }
+
+    private void SetGrassEffect(ElementType characterType)
+    {
+        if (characterType == ElementType.Grass)
+        {
+            icon.sprite = buffSprite;
+            text.text = "Buff";
+            detail.text = grassTileBuff.DisplayKeywordDescription();
+        }
+        else
+        {
+            icon.sprite = debuffSprite;
+            text.text = "Debuff";
+            detail.text = grassTileDebuff.DisplayKeywordDescription();
         }
     }
 }
