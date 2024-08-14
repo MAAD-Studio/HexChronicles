f70abf9d4e3ee2d4ec6b7b7967e9b7a309291a7d
Aria Lian <arialian0829@gmail.com>
2024-08-14 02:16:42 -0400

    Modified skill selection
    Modified to let player to change skill reward selection,  - SkillRewardCard & VictoryReward
    skill being changed when continue to world map - VictoryScreen

--- Assets/Game/UI/Scripts/Post_Battle/VictoryReward.cs
+++ Assets/Game/UI/Scripts/Post_Battle/VictoryReward.cs
@@ -8,6 +8,10 @@ public class VictoryReward : MonoBehaviour
     [SerializeField] private GameObject skillCard;
     [SerializeField] private Transform upgradePanel;
     private List<ActiveSkillSO> rewardSkills = new List<ActiveSkillSO>();
+    private List<SkillRewardCard> rewardCards = new List<SkillRewardCard>();
+
+    private ActiveSkillSO selectedSkill;
+    public ActiveSkillSO SelectedSkill => selectedSkill;
 
     public void SpawnCards()
     {
@@ -20,39 +24,74 @@ public class VictoryReward : MonoBehaviour
             card.GetComponent<UpgradeCard>().InitializeUI();
         }*/
 
-        // Spawn 3 skill cards ---- TEMPORARY
+        // Spawn 3 random skill cards
         for (int i = 0; i < 3; i++)
         {
-            GameObject card = Instantiate(skillCard, upgradePanel);
-
-            // Spawn a random element skill card
+            // Select a random element skill
             ElementType elementType = (ElementType)Random.Range(0, 3);
-            SkillRewardCard rewardCard = card.GetComponent<SkillRewardCard>();
-
             ActiveSkillSO skill = ActiveSkillCollection.Instance.GetRandomSkillReward(elementType);
 
             if (skill == null)
             {
-                Debug.LogError($"No more {elementType} skills to reward");
-                Destroy(card);
-                return;
+                Debug.Log($"No more {elementType} skills to reward");
+
+                // Change to the next elementType (between 0, 1, 2)
+                elementType = (ElementType)(((int)elementType + 1) % 3);
+                skill = ActiveSkillCollection.Instance.GetRandomSkillReward(elementType);
+
+                if (skill == null)
+                {
+                    Debug.Log($"No more {elementType} skills to reward");
+
+                    // Change to the next elementType
+                    elementType = (ElementType)(((int)elementType + 1) % 3);
+                    skill = ActiveSkillCollection.Instance.GetRandomSkillReward(elementType);
+
+                    if (skill == null)
+                    {
+                        Debug.LogError($"No more skills left!!!");
+                        return;
+                    }
+                }
             }
 
             // If the skill is already in the reward, get another skill
-            /*while (rewardSkills.Contains(skill))
+            /*while (rewardSkills.Contains(skill) && ActiveSkillCollection.Instance.RemainSkillsCount > 2)
             {
                 skill = ActiveSkillCollection.Instance.GetRandomSkillReward(elementType);
+                Debug.Log("Duplicated skill, getting another one");
             }*/
 
+            // Spawn card
+            GameObject card = Instantiate(skillCard, upgradePanel);
+            SkillRewardCard rewardCard = card.GetComponent<SkillRewardCard>();
+            rewardCard.SetSkillDisplay(skill, this);
+
             rewardSkills.Add(skill);
-            rewardCard.SetSkillDisplay(skill);
+            rewardCards.Add(rewardCard);
         }
 
         rewardSkills.Clear();
     }
 
+    public void OnSkillSelected(ActiveSkillSO skill, SkillRewardCard rewardCard)
+    {
+        selectedSkill = skill;
+
+        foreach (SkillRewardCard card in rewardCards)
+        {
+            if (card != rewardCard)
+            {
+                card.OnUnselected();
+            }
+        }
+    }
+
     private void ResetCards()
     {
+        rewardCards.Clear();
+        selectedSkill = null;
+
         foreach (Transform child in upgradePanel)
         {
             Destroy(child.gameObject);
